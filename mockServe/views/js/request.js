c19433c5d7b3d67930efd45a806122a8e02ae07c
LaoCheng-cjs <49183028+LaoCheng-cjs@users.noreply.github.com>
2020-05-13 17:31:12 +0800

    新增

--- mockServe/views/js/request.js
+++ mockServe/views/js/request.js
@@ -1,19 +1,10 @@
-(function () {
+(function (win) {
     // 定义请求
     var request = function (options) {
-        var option = options || {},
-            formal = {
-                url: '/__api/getApiList',
-                method: 'POST',
-                dataType: 'JSON',
-                contentType: "application/json;charset=utf-8",
-                data: JSON.stringify({
-                    myTest: 2222222222
-                }),
-                success() {
-
-                }
-            }
+        var option = options || {};
+        option.dataType = 'JSON'
+        option.contentType = "application/json;charset=utf-8";
+        option.data = JSON.stringify(option.data?option.data:{})
         if (!option.url && typeof option.url != 'string') {
             console.error(['视图view 请求的url不存在：' + option.url])
             return;
@@ -21,9 +12,32 @@
         $.ajax(option)
     }
     // 定义
-    window.mockApi = {
-        index() { // index请求
-            request('')
+    win.mockApi = {
+        getListApi(cb) { // 获取请求接口列表
+            request({
+                url: '/__api/getConfig',
+                method: 'POST',
+                success(res) {
+                    console.log(1221,'');
+                    if (typeof cb === 'function') {
+                        cb(res)
+                    }
+                }
+            })
+        },
+        getAllApi(cb) { // 请求所有
+            request({
+                url: '/__api/getAllApi',
+                method: 'POST',
+                success(res) {
+                    if (typeof cb === 'function') {
+                        cb(res)
+                    }
+                }
+            })
+        },
+        getDoc() { // 获取操作文档及代码
+
         }
     }
-})()
+})(window)
--- mockServe/webServe.js
+++ mockServe/webServe.js
@@ -5,6 +5,8 @@ var path = require('path')
 var express = require('express')
 var router = express.Router()
 import { read } from "node-yaml";
+var fs = require('fs')
+var path = require('path')
 var request = {}
 // 加载配置文件
 read(path.join(__dirname, './config/serve.yml'))
@@ -17,15 +19,16 @@ read(path.join(__dirname, './config/serve.yml'))
 })
 // console.log(, '');
 // Yml.load(path.join(__dirname, './config/serve.yml'))
-router.all('/__api/getApiList', function (req, res, next) {
-    console.log(request.getApiList, '');
-    console.log(req.body)
-    console.log(req.query)
-    console.log(req.params)
-    res.send({a:121})
+router.all('/__api/getConfig', function (req, res, next) { // 获取当前配置文件
+    var data = JSON.parse(fs.readFileSync(path.join(__dirname, './config/index.json')).toString())
+    res.json(data)
     next()
 })
-
+router.all('/__api/getAllApi', function (req, res, next) { // 添加后的接口地址
+    var data = JSON.parse(fs.readFileSync(path.join(__dirname, './config/addMock.json')).toString())
+    res.json(data)
+    next()
+})
 
 module.exports = router
 
